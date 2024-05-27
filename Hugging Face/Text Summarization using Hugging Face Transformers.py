b0391c018fcf55cfcc112076ce793a87a9795b53
Bharat Kumar Mallavarapu <ybharatk7@gmail.com>
2024-05-27 10:58:07 -0500

    Update Text Summarization using Hugging Face Transformers.py

--- Hugging Face/Text Summarization using Hugging Face Transformers.py
+++ Hugging Face/Text Summarization using Hugging Face Transformers.py
@@ -7,26 +7,24 @@ from transformers import pipeline
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 # Your text to summarize
 # Source: https://learn.microsoft.com/en-us/training/modules/intro-to-git/1-what-is-vc
-text = """ To understand Git, you have to understand the terminology. Here's a short list of terms that Git users frequently use. Don't be concerned about the details for now; all these terms will become familiar as you work your way through the exercises in this module.
-
-Working tree: The set of nested directories and files that contain the project that's being worked on.
-
-Repository (repo): The directory, located at the top level of a working tree, where Git keeps all the history and metadata for a project. Repositories are almost always referred to as repos. A bare repository is one that isn't part of a working tree; it's used for sharing or backup. A bare repo is usually a directory with a name that ends in .git—for example, project.git.
-
-Hash: A number produced by a hash function that represents the contents of a file or another object as a fixed number of digits. Git uses hashes that are 160 bits long. One advantage to using hashes is that Git can tell whether a file has changed by hashing its contents and comparing the result to the previous hash. If the file time-and-date stamp is changed, but the file hash isn’t changed, Git knows the file contents aren’t changed.
-
-Object: A Git repo contains four types of objects, each uniquely identified by an SHA-1 hash. A blob object contains an ordinary file. A tree object represents a directory; it contains names, hashes, and permissions. A commit object represents a specific version of the working tree. A tag is a name attached to a commit.
-
-Commit: When used as a verb, commit means to make a commit object. This action takes its name from commits to a database. It means you are committing the changes you have made so that others can eventually see them, too.
-
-Branch: A branch is a named series of linked commits. The most recent commit on a branch is called the head. The default branch, which is created when you initialize a repository, is called main, often named master in Git. The head of the current branch is named HEAD. Branches are an incredibly useful feature of Git because they allow developers to work independently (or together) in branches and later merge their changes into the default branch.
-
-Remote: A remote is a named reference to another Git repository. When you create a repo, Git creates a remote named origin that is the default remote for push and pull operations.
-
-Commands, subcommands, and options: Git operations are performed by using commands like git push and git pull. git is the command, and push or pull is the subcommand. The subcommand specifies the operation you want Git to perform. Commands frequently are accompanied by options, which use hyphens (-) or double hyphens (--). For example, git reset --hard.
-
-These terms and others, like push and pull, will make more sense shortly. But you have to start somewhere, and you might find it helpful to come back and review this glossary of terms after you finish the module.
-
+text = """ Amazon has introduced a new Linux-based open-source operating system called Bottlerocket. 
+The purpose of this OS is that it is specifically built to run containers. 
+It runs in Amazon Elastic Kubernetes Service (EKS), Amazon Elastic Container Service (ECS). 
+Bottlerocket is written primarily with memory-safe languages like Rust and Go. 
+It mainly focuses on security and durability. Many containerized applications today are run 
+on general-purpose operating systems that are updated package-by-package, 
+which makes OS updates difficult to automate. To only run containers on a Linux system, 
+a complete Linux distribution is not always necessary. Bottlerocket only includes essential 
+software to run containers. Bottlerocket reduces the footprint of the underlying OS, aiding overall performance. 
+Second, it limits the exposure to potential attacks, as many of the tools leveraged by would-be attackers aren’t there. 
+Instead of a package update system, Bottlerocket uses a simple, image-based model that allows for a rapid & complete rollback if necessary. 
+This removes opportunities for conflicts and breakage, making it easier for you to apply fleet-wide updates using orchestrators such as 
+EKS confidently. Bottlerocket’s open development model enables customers and partners to produce custom builds, such as builds that support 
+their preferred orchestrators. Changes in these custom builds can be added to the Bottlerocket open-source project.
+Bottlerocket also uses a primarily read-only file system. Its integrity is checked at boot time via dm-verity. For additional security measures, 
+SSH access is also discouraged and is only available through the admin container.
+You can automate updates to Bottlerocket by using an orchestration service like Amazon EKS. Amazon also claims that including only the essential 
+software to run containers reduces the attack surface compared to general-purpose Linux distributions.
 """
 
 # Generate the summary
